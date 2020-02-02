1987322e34f54c79f7bc647b0bc8375855fad32e
Estecka <estecka@galacsys.com>
2020-02-02 17:25:02 +0100

    Added mxv.

--- matrix.h
+++ matrix.h
@@ -13,20 +13,27 @@
 #ifndef MATRIX_H
 # define MATRIX_H
 
+# include "vector.h"
+
 typedef float*const	*t_mx;
 typedef float		t_mx4[4][4];
 typedef float		t_mx3[3][3];
 typedef float		t_mx2[2][2];
 
-void	mxtrans(t_mx matrix, t_mx destination, unsigned int rank);
+void		mxtrans(t_mx matrix, t_mx destination, unsigned int rank);
 
-void	mx3adj(t_mx3 matrix, t_mx3 destination);
-void	mx3adjt(t_mx3 transposed, t_mx3 destination);
+void		mx3adj(t_mx3 matrix, t_mx3 destination);
+void		mx3adjt(t_mx3 transposed, t_mx3 destination);
 
-float	mx2det(t_mx2 matrix);
-float	mx3det(t_mx3 matrix);
-void	mx2inv(t_mx2 m, t_mx2 mi)
-void	mx2inv(t_mx2 m, t_mx2 mi)
+float		mx2det(t_mx2 matrix);
+float		mx3det(t_mx3 matrix);
+void		mx2inv(t_mx2 matrix, t_mx2 destination)
+void		mx2inv(t_mx2 matrix, t_mx2 destination)
 
+union u_v2	mx2v2(t_mx2 matrix, t_v2 vector);
+union u_v3	mx3v2(t_mx3 matrix, t_v2 vector);
+union u_v3	mx3v3(t_mx3 matrix, t_v3 vector);
+union u_v4	mx4v3(t_mx4 matrix, t_v3 vector);
+union u_v4	mx4v4(t_mx4 matrix, t_v4 vector);
 
 #endif
