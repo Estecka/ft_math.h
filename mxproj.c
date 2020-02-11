219100dd2954790c35e335db102a0f7d3ff01833
Estecka <estecka@galacsys.com>
2020-02-11 10:09:12 +0100

    Changed frustrum bbox definition.

--- mxproj.c
+++ mxproj.c
@@ -19,10 +19,10 @@
 ** @param const t_bbox *box The dimensions of the frustrum.
 ** 	box.min.x Left
 ** 	box.min.y Bottom
-** 	box.min.z Near
+** 	box.min.z Far
 ** 	box.max.x Right
 ** 	box.max.y Top
-** 	box.max.x Far
+** 	box.max.x Near
 */
 
 extern void	mxfrust(t_mx4 this, const t_bbox *box)
@@ -33,14 +33,14 @@ extern void	mxfrust(t_mx4 this, const t_bbox *box)
 
 	iwidth = 1 / (box->max.x - box->min.x);
 	iheight = 1 / (box->max.y - box->min.y);
-	idepth = 1 / (box->max.z - box->min.z);
+	idepth = 1 / (box->min.z - box->max.z);
 	mx4init(this);
-	this[0][0] = -2 * box->min.z * iwidth;
+	this[0][0] = -2 * box->max.z * iwidth;
 	this[0][2] = (box->min.x + box->max.x) * iwidth;
-	this[1][1] = -2 * box->min.z * iheight;
+	this[1][1] = -2 * box->max.z * iheight;
 	this[2][1] = (box->min.y + box->max.y) * iheight;
-	this[2][2] = -(box->min.z + box->max.z) * idepth;
-	this[3][2] = 2 * box->min.z * box->max.z * idepth;
+	this[2][2] = -(box->max.z + box->min.z) * idepth;
+	this[3][2] = 2 * box->max.z * box->min.z * idepth;
 	this[3][3] = 0;
 	this[2][3] = -1;
 }
