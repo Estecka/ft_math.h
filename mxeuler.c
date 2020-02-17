ce505021d115a228cc843bb5967bb9b36985fd9f
Adrien Baur <abaur@student.42.fr>
2020-02-17 16:57:24 +0100

    Changed MxEuler to not initialize the whole matrix
    Fixed the 4x4 euler matrix.

--- mxeuler.c
+++ mxeuler.c
@@ -14,8 +14,9 @@
 #include "matrix.h"
 
 /*
-** Initializes a matrix as the rotation matrix around a single axis..
-** @param t_mx4 this The matrix to initialize.
+** Changes the 3x3 portion of a 4x4 matrix to a rotation around a single axis.
+** The rest of the matrix is kept unchanged
+** @param t_mx4 this The matrix to alter.
 ** @param float a The rotation angle in radiant.
 */
 
@@ -26,11 +27,15 @@ extern void	mx4eulerx(t_mx4 this, float a)
 
 	c = cos(a);
 	s = sin(a);
-	mx4init(this);
+	this[0][0] = 1;
+	this[0][1] = 0;
+	this[0][2] = 0;
+	this[1][0] = 0;
 	this[1][1] = c;
-	this[2][2] = c;
-	this[2][1] = -s;
 	this[1][2] = s;
+	this[2][0] = 0;
+	this[2][1] = -s;
+	this[2][2] = c;
 }
 
 extern void	mx4eulery(t_mx4 this, float a)
@@ -40,11 +45,15 @@ extern void	mx4eulery(t_mx4 this, float a)
 
 	c = cos(a);
 	s = sin(a);
-	mx4init(this);
 	this[0][0] = c;
-	this[2][2] = c;
-	this[2][0] = s;
+	this[0][1] = 0;
 	this[0][2] = -s;
+	this[1][0] = 0;
+	this[1][1] = 1;
+	this[1][2] = 0;
+	this[2][0] = s;
+	this[2][1] = 0;
+	this[2][2] = c;
 }
 
 extern void	mx4eulerz(t_mx4 this, float a)
@@ -54,16 +63,20 @@ extern void	mx4eulerz(t_mx4 this, float a)
 
 	c = cos(a);
 	s = sin(a);
-	mx4init(this);
 	this[0][0] = c;
-	this[1][1] = c;
-	this[1][0] = -s;
 	this[0][1] = s;
+	this[0][2] = 0;
+	this[1][0] = -s;
+	this[1][1] = c;
+	this[1][2] = 0;
+	this[2][0] = 0;
+	this[2][1] = 0;
+	this[2][2] = 1;
 }
 
 /*
-** Initializes a rotation matrix.
-** @param t_mx4 this The matrix to initialize.
+** Changes the 3x3 portion of a 4x4 matrix to a rotation around three axes.
+** @param t_mx4 this The matrix to alter.
 ** @param const t_v3 *a The rotation angles in radiant.
 */
 
@@ -78,14 +91,13 @@ extern void mx4euler(t_mx4 this, const t_v3 *a)
 	s.x = sin(a->x);
 	s.y = sin(a->y);
 	s.z = sin(a->z);
-	mx4init(this);
-	this[0][0] = +c.z * ((c.y * c.x) + (s.y * s.x));
-	this[1][0] = -s.z * ((c.y * c.x) - (s.y * s.x));
-	this[2][0] = (c.y * s.x) + (s.y * c.x);
+	this[0][0] = +c.z * ((c.y * c.x) - (s.y * s.x));
 	this[0][1] = s.z;
-	this[1][1] = c.z;
-	this[2][1] = 0;
 	this[0][2] = -c.z * ((s.y * c.x) + (c.y * s.x));
+	this[1][0] = -s.z * ((c.y * c.x) + (s.y * s.x));
+	this[1][1] = c.z;
 	this[1][2] = +s.z * ((s.y * c.x) - (c.y * s.x));
-	this[2][3] = (c.y * c.x) - (s.y * s.x);
+	this[2][0] = (c.y * s.x) + (s.y * c.x);
+	this[2][1] = 0;
+	this[2][2] = (c.y * c.x) - (s.y * s.x);
 }
